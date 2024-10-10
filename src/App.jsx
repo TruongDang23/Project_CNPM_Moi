447c174cf9e671b4c406ce0c2c5e1bc81545f9ad
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2024-10-10 22:31:56 +0700

    sườn dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Login from '../src/pages/admin/login/Login.jsx'
 import Welcome from './pages/system/welcome/Welcome.jsx'
+import DashBoard from './pages/admin/dashboard/DashBoard.jsx'
 
 function App() {
-  return <Welcome />
+  return <DashBoard />
 }
 
 export default App
