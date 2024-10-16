3ddbd4c4d32b2e88d49ea2007ad74a306c351fc2
BaoVu0712 <91738440+BaoVu0712@users.noreply.github.com>
2024-10-16 21:47:48 +0700

    FE Quản Lý MC

---
+++ src/data/someMCData.js
@@ -0,0 +1,49 @@
+const someMCData = [
+  {
+    MaMC: 'M004',
+    HoTen: 'Lê Minh Tuấn',
+    SDT: '0945678901',
+    KinhNghiem: 10,
+    TinhTrang: false,
+    Gia: 1800000,
+    DanhGia: []
+  },
+  {
+    MaMC: 'M001',
+    HoTen: 'Nguyễn Văn A',
+    SDT: '0912345678',
+    KinhNghiem: 5,
+    TinhTrang: true,
+    Gia: 2000000,
+    DanhGia: []
+  },
+  {
+    MaMC: 'M002',
+    HoTen: 'Trần Thị B',
+    SDT: '0901112233',
+    KinhNghiem: 3,
+    TinhTrang: true,
+    Gia: 2500000,
+    DanhGia: []
+  },
+  {
+    MaMC: 'M003',
+    HoTen: 'Lê Văn C',
+    SDT: '0912345679',
+    KinhNghiem: 7,
+    TinhTrang: false,
+    Gia: 3000000,
+    DanhGia: []
+  },
+  {
+    MaMC: 'M005',
+    HoTen: 'Phạm Thị D',
+    SDT: '0902223344',
+    KinhNghiem: 4,
+    TinhTrang: true,
+    Gia: 2200000,
+    DanhGia: []
+  }
+]
+
+export default someMCData;
