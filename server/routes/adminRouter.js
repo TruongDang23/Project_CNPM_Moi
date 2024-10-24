789f10ebd59b7b5fe746d2ff7f7861735ef9ea29
Lê Thành Vinh <lethanhvinhspk@gmail.com>
2024-10-24 21:22:45 +0700

    base branch

--- server/routes/adminRouter.js
+++ server/routes/adminRouter.js
@@ -1,10 +1,8 @@
 //import express framework (bắt buộc)
 import express from 'express'
-import cors from 'cors'
 import authController from '../controllers/authController.js'
 
 const adminRouter = express.Router()
-adminRouter.use(cors())
 
 adminRouter.post('/login', authController.login)
 
